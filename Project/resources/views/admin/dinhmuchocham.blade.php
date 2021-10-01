fd2e9b664ea2d169307aaa4fc40bc8d1297daa60
KhanhHoa <khanhhoabmt9.9.9x@gmail.com>
2021-10-01 15:17:36 +0700

    commit

--- Project/resources/views/admin/dinhmuchocham.blade.php
+++ Project/resources/views/admin/dinhmuchocham.blade.php
@@ -80,7 +80,8 @@
                       </div>
                       </form>
 
-                      <form method= "POST" action="/xoa" class="form-horizontal form-label-left">
+                      <form method= "POST" action="../xoa" class="form-horizontal form-label-left">
+                      @csrf
                       <div class="col-md-12 col-sm-12 " style="font-size: medium; margin-top: 30px;">
                         <div class="x_panel">
                           <div class="x_title">
