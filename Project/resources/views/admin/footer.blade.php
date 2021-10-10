65a0d7b697f098bcd2f7b71242e530eba7d47cf6
DaoHongPhuc <hongphuc1694@gmail.com>
2021-10-10 12:47:01 +0700

    Fix css error - 2th

--- Project/resources/views/admin/footer.blade.php
+++ Project/resources/views/admin/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- jQuery -->
-<script src="../../js/jquery.min.js"></script>
+<script src="js/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src="../../js/bootstrap.bundle.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
 <!-- Custom Theme Scripts -->
-<script src="../../js/custom.min.js"></script>
+<script src="js/custom.min.js"></script>
