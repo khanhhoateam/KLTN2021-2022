b14153f2839ef2e230f4aae352ccf38500fd41db
DaoHongPhuc <hongphuc1694@gmail.com>
2021-10-10 10:40:21 +0700

    Seperate routes

---
+++ Project/app/RouteCustom/hoa_routes.php
@@ -0,0 +1,11 @@
+<?php
+
+Route::prefix('thiet-lap-dinh-muc')->group(function () {
+    Route::get('/', [HocHamController::class, 'create']);
+    Route::prefix('hoc-ham')->group(function () {
+        Route::get('/', [HocHamController::class, 'create']);
+        Route::post('/', [HocHamController::class, 'temporary_table']);
+        Route::get('xoa/{HocHamTamID}', [HocHamController::class, 'del_temp_table']);
+        Route::get('luu', [HocHamController::class, 'store']);
+
+});
--- Project/routes/web.php
+++ Project/routes/web.php
@@ -2,37 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HocHamController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::prefix('admin')->group(function () {
+
     Route::get('/', function () {
         return view('admin.home');
     });
 
-    Route::prefix('thiet-lap-dinh-muc')->group(function () {
-
-        Route::get('/', [HocHamController::class, 'create']);
-        Route::get('/test', [HocHamController::class, 'createtest']);
-
-        Route::prefix('hoc-ham')->group(function () {
-
-            Route::get('/', [HocHamController::class, 'create']);
-
-            Route::post('/', [HocHamController::class, 'temporary_table']);
-
-            Route::get('xoa/{HocHamTamID}', [HocHamController::class, 'del_temp_table']);
-
-            Route::get('luu', [HocHamController::class, 'store']);
-
-    });
+    include_once app_path() . "/RouteCustom/phuc_routes.php";
+    include_once app_path() . "/RouteCustom/hoa_routes.php";
 
 });
